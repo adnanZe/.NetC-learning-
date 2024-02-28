ad3414c6bcaec927c2531546d464f8b2f008032c
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-28 18:20:59 +0200

    learning c#

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,16 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This is the first line.");
-Console.Write("This ");
-Console.Write("is the ");
-Console.WriteLine("second line.");
-Console.WriteLine(123);
-Console.WriteLine(0.25F);
-Console.WriteLine(12.39816m);
-Console.WriteLine(true);
-Console.WriteLine(false);
+﻿// // // // // See https://aka.ms/new-console-template for more information
+// // // // Console.WriteLine("This is the first line.");
+// // // // Console.Write("This ");
+// // // // Console.Write("is the ");
+// // // // Console.WriteLine("second line.");
+// // // // Console.WriteLine(123);
+// // // // Console.WriteLine(0.25F);
+// // // // Console.WriteLine(12.39816m);
+// // // // Console.WriteLine(true);
+// // // // Console.WriteLine(false);
 
-Console.WriteLine("123");
-Console.WriteLine(123);
+// // // // Console.WriteLine("123");
+// // // // Console.WriteLine(123);
 
-Console.WriteLine("true");
-Console.WriteLine(true);
+// // // // Console.WriteLine("true");
+// // // // Console.WriteLine(true);
+
+// // // // string firstName;
+// // // // int age;
+// // // // char dadFirstLetter;
+// // // // decimal salary;
+// // // // bool isEmployed;
+
+// // // // firstName = "Joe";
+// // // // age = 20;
+// // // // dadFirstLetter = 'B';
+// // // // salary = 5555555.5m;
+// // // // isEmployed = true;
+
+// // // // Console.WriteLine("My first name is " + firstName + " age is " + age + " dad first letter " + dadFirstLetter + " salary is " + salary + " " + " is hired ? " + isEmployed);
+
+// // // // firstName = "Bill";
+
+// // // // Console.WriteLine("My first name is " + firstName);
+
+// // // string firstName;
+// // // firstName = "Bob";
+// // // Console.WriteLine(firstName);
+// // // firstName = "Liem";
+// // // Console.WriteLine(firstName);
+// // // firstName = "Isabella";
+// // // Console.WriteLine(firstName);
+// // // firstName = "Yasmin";
+// // // Console.WriteLine(firstName);
+
+// // // string lastName = "Black";
+// // // Console.WriteLine(lastName);
+
+// // var message;
+// // message = "Hello world!";
+
+// // Console.WriteLine(message);
+
+// // message = 10.703m;
+
+// // Console.WriteLine(message);
+// // Console.WriteLine(message);
+
+// string name = "Bob";
+// int numberMessage = 3;
+// double temperature = 34.4;
+
+// Console.WriteLine("Hello " + name + "! You have " + numberMessage + " in your inbox. The temperature is " + temperature + " celsius.");
+
+// Console.WriteLine(34.40M);
+
+// decimal x = 12.3m;
