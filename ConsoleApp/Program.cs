e91a3357e679aa3a6b8288ead1a8c7c73fb30c16
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-28 18:44:38 +0200

    Combine strings using string concatenation

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,93 +1,98 @@
-﻿// // // // // // See https://aka.ms/new-console-template for more information
-// // // // // Console.WriteLine("This is the first line.");
-// // // // // Console.Write("This ");
-// // // // // Console.Write("is the ");
-// // // // // Console.WriteLine("second line.");
-// // // // // Console.WriteLine(123);
-// // // // // Console.WriteLine(0.25F);
-// // // // // Console.WriteLine(12.39816m);
-// // // // // Console.WriteLine(true);
-// // // // // Console.WriteLine(false);
+﻿// // // // // // // See https://aka.ms/new-console-template for more information
+// // // // // // Console.WriteLine("This is the first line.");
+// // // // // // Console.Write("This ");
+// // // // // // Console.Write("is the ");
+// // // // // // Console.WriteLine("second line.");
+// // // // // // Console.WriteLine(123);
+// // // // // // Console.WriteLine(0.25F);
+// // // // // // Console.WriteLine(12.39816m);
+// // // // // // Console.WriteLine(true);
+// // // // // // Console.WriteLine(false);
 
-// // // // // Console.WriteLine("123");
-// // // // // Console.WriteLine(123);
+// // // // // // Console.WriteLine("123");
+// // // // // // Console.WriteLine(123);
 
-// // // // // Console.WriteLine("true");
-// // // // // Console.WriteLine(true);
+// // // // // // Console.WriteLine("true");
+// // // // // // Console.WriteLine(true);
+
+// // // // // // string firstName;
+// // // // // // int age;
+// // // // // // char dadFirstLetter;
+// // // // // // decimal salary;
+// // // // // // bool isEmployed;
+
+// // // // // // firstName = "Joe";
+// // // // // // age = 20;
+// // // // // // dadFirstLetter = 'B';
+// // // // // // salary = 5555555.5m;
+// // // // // // isEmployed = true;
+
+// // // // // // Console.WriteLine("My first name is " + firstName + " age is " + age + " dad first letter " + dadFirstLetter + " salary is " + salary + " " + " is hired ? " + isEmployed);
+
+// // // // // // firstName = "Bill";
+
+// // // // // // Console.WriteLine("My first name is " + firstName);
 
 // // // // // string firstName;
-// // // // // int age;
-// // // // // char dadFirstLetter;
-// // // // // decimal salary;
-// // // // // bool isEmployed;
+// // // // // firstName = "Bob";
+// // // // // Console.WriteLine(firstName);
+// // // // // firstName = "Liem";
+// // // // // Console.WriteLine(firstName);
+// // // // // firstName = "Isabella";
+// // // // // Console.WriteLine(firstName);
+// // // // // firstName = "Yasmin";
+// // // // // Console.WriteLine(firstName);
 
-// // // // // firstName = "Joe";
-// // // // // age = 20;
-// // // // // dadFirstLetter = 'B';
-// // // // // salary = 5555555.5m;
-// // // // // isEmployed = true;
+// // // // // string lastName = "Black";
+// // // // // Console.WriteLine(lastName);
 
-// // // // // Console.WriteLine("My first name is " + firstName + " age is " + age + " dad first letter " + dadFirstLetter + " salary is " + salary + " " + " is hired ? " + isEmployed);
+// // // // var message;
+// // // // message = "Hello world!";
 
-// // // // // firstName = "Bill";
+// // // // Console.WriteLine(message);
 
-// // // // // Console.WriteLine("My first name is " + firstName);
+// // // // message = 10.703m;
 
-// // // // string firstName;
-// // // // firstName = "Bob";
-// // // // Console.WriteLine(firstName);
-// // // // firstName = "Liem";
-// // // // Console.WriteLine(firstName);
-// // // // firstName = "Isabella";
-// // // // Console.WriteLine(firstName);
-// // // // firstName = "Yasmin";
-// // // // Console.WriteLine(firstName);
+// // // // Console.WriteLine(message);
+// // // // Console.WriteLine(message);
 
-// // // // string lastName = "Black";
-// // // // Console.WriteLine(lastName);
+// // // string name = "Bob";
+// // // int numberMessage = 3;
+// // // double temperature = 34.4;
 
-// // // var message;
-// // // message = "Hello world!";
+// // // Console.WriteLine("Hello " + name + "! You have " + numberMessage + " in your inbox. The temperature is " + temperature + " celsius.");
 
-// // // Console.WriteLine(message);
+// // // Console.WriteLine(34.40M);
 
-// // // message = 10.703m;
+// // // decimal x = 12.3m;
 
-// // // Console.WriteLine(message);
-// // // Console.WriteLine(message);
+// // Console.WriteLine("Hello\nWorld!");
+// // Console.WriteLine("Hello\tWorld!");
 
-// // string name = "Bob";
-// // int numberMessage = 3;
-// // double temperature = 34.4;
+// // Console.WriteLine("Hello \"World\"!");
 
-// // Console.WriteLine("Hello " + name + "! You have " + numberMessage + " in your inbox. The temperature is " + temperature + " celsius.");
+// // Console.WriteLine("c:\\source\\repos");
 
-// // Console.WriteLine(34.40M);
+// Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+// Console.WriteLine("Invoice: 1021\t\tComplete!");
+// Console.WriteLine("Invoice: 1022\t\tComplete!");
+// Console.WriteLine("\nOutput Directory:\t");
 
-// // decimal x = 12.3m;
+// // Console.WriteLine(@"    c:\source\repos
+// //         (this is where your code goes)");
 
-// Console.WriteLine("Hello\nWorld!");
-// Console.WriteLine("Hello\tWorld!");
+// Console.WriteLine(@"c:\invoices");
 
-// Console.WriteLine("Hello \"World\"!");
+// // Kon'nichiwa World
+// Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
 
-// Console.WriteLine("c:\\source\\repos");
+// // To generate Japanese invoices:
+// // Nihon no seikyū-sho o seisei suru ni wa:
+// Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// // User command to run an application
+// Console.WriteLine(@"c:\invoices\app.exe -j");
 
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-
-// Console.WriteLine(@"    c:\source\repos
-//         (this is where your code goes)");
-
-Console.WriteLine(@"c:\invoices");
-
-// Kon'nichiwa World
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
-
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
+string firstName = "Bob";
+string greeting  = "Hello";
+string message = greeting + " " + firstName + "!";
+Console.WriteLine(message); 
