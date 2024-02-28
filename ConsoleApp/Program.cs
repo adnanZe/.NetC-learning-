b92b2b5fc5a0e03dc7bc464432adfec74c024fdf
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-28 17:48:57 +0200

    printing literal values

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-Console.Write("Congratulations!");
-// Console.WriteLine("The current time is " + DateTime.Now);
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.Write("This ");
+Console.Write("is the ");
+Console.WriteLine("second line.");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
