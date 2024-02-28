e9945d9da1156f2e23183aab8646960c9fe5bdac
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-28 21:23:50 +0200

    Perform math operations

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -139,6 +139,38 @@
 // int widgetsSold = 7;
 // Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
 
-string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+// string firstName = "Bob";
+// int widgetsSold = 7;
+// Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+// int sum = 7 + 5;
+// int difference = 7 - 5;
+// int product = 7 * 5;
+// int quotient = 7 / 5;
+
+// Console.WriteLine("Sum: " + sum);
+// Console.WriteLine("Difference: " + difference);
+// Console.WriteLine("Product: " + product);
+// Console.WriteLine("Quotient: " + quotient);
+
+// decimal decimalQuotient = 7.0m / 5;
+// decimal decimalQuotient = 7 / 5.0m;
+// decimal decimalQuotient = 7.0m / 5.0m;
+// int decimalQuotient = 7 / 5.0m;
+// int decimalQuotient = 7.0m / 5;
+// int decimalQuotient = 7.0m / 5.0m;
+// decimal decimalQuotient = 7 / 5;
+// Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+// int first = 7;
+// int second = 5;
+// decimal quotient = first / (decimal)second;
+// Console.WriteLine(quotient);
+
+// Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+// Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
