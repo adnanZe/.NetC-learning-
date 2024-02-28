0670673e408648148a8bbe1ae63f3792ea46fffd
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-28 16:36:00 +0200

    write in console learning

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+// Console.WriteLine("The current time is " + DateTime.Now);
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
