636d9f2e3e708e81a13af63a9e1c53510ad6811e
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-29 10:45:48 +0200

    Create nested decision logic with if, else if, and else

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -215,6 +215,16 @@
 
 // Console.Write($"The temperature is {celsius} Celsius \n");
 
+
+// string message = "The quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("What does the fox say?");
+// }
+
 Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
@@ -227,31 +237,31 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3))
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new laptop!");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a trip for two!");
 }
-
-// string message = "The quick brown fox jumps over the lazy dog.";
-// bool result = message.Contains("dog");
-// Console.WriteLine(result);
-
-// if (message.Contains("fox"))
-// {
-//     Console.WriteLine("What does the fox say?");
-// }
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
