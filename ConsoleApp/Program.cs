f14d213ed5b8f892b24eface8d9b8265804122ac
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-03-02 18:26:53 +0200

    [working] with arrays

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -313,20 +313,35 @@
 // Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 // Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
 
-string[] names = ["Bob", "Conrad", "Grant"];
-foreach (string name in names)
-{
-    Console.WriteLine(name);
-}
+// string[] names = ["Bob", "Conrad", "Grant"];
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
 
-int[] inventory = [200, 450, 700, 175, 250];
-int sum = 0;
-int bin = 0;
-foreach (int items in inventory)
-{
-    sum += items;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-}
+// int[] inventory = [200, 450, 700, 175, 250];
+// int sum = 0;
+// int bin = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
 
-Console.WriteLine($"We have {sum} items in inventory.");
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+// B123
+// C234
+// A345
+// C15
+// B177
+// G3003
+// C235
+// B179
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
