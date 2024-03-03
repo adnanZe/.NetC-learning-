8998792ec4af38aedbd5e5ea508a93d94ebc4727
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-03-03 12:07:08 +0200

    [challenge]

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -340,8 +340,11 @@
 // C235
 // B179
 
-string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
 foreach (string orderID in orderIDs)
 {
-    Console.WriteLine(orderID);
+    if (orderID.StartsWith('B'))
+    {
+        Console.WriteLine(orderID);
+    }
 }
