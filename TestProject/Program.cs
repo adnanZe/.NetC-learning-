faa1096d03e7125704e3fe93be52299341a04119
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-03-05 22:19:02 +0200

    Return values and input parameters of methods

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Random dice = new Random();
+// // int roll = dice.Next();
+
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+
+// Console.WriteLine(dice.Next(1, 7));
+
+// int number = 7;
+// string text = "seven";
+
+// Console.WriteLine(number);
+// Console.WriteLine();
+// Console.WriteLine(text);
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
